4ca05eac2d1f3873a15c2e5f78662d9eafb71cb5
Jon Bright <jon@siliconcircus.com>
2023-08-02 22:31:01 +0200

    Remove inadvertent extra ptprint directory.

--- ptprint.sh
+++ ptprint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR=$(dirname $0)
-echo "/usr/bin/screen -S PTPRINT -d -m ${DIR}/ptprint/start_ptprint.sh" |/usr/bin/at now
+echo "/usr/bin/screen -S PTPRINT -d -m ${DIR}/start_ptprint.sh" |/usr/bin/at now
